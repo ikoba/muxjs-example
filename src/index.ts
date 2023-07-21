8292d16a5f7301b265b8a230807fcb5b871631bd
ikoba <i.koba.jp@gmail.com>
2023-07-21 21:20:36 +0900

    Add muxjs.mp4.tools.inspect example

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import * as muxjs from "mux.js";
+console.log('↓ muxjs');
 console.log(muxjs);
 
 document.getElementById('file').addEventListener('change', selectFile)
 
 async function selectFile(e: any) {
   const file = e.target.files[0];
+  console.log('↓ file');
   console.log(file);
 
   const buf = await readVideo(file);
   const bytes = new Uint8Array(buf);
-  const res = muxjs.mp4.probe.tracks(bytes);
-  console.table(res);
+  const probeResult = muxjs.mp4.probe.tracks(bytes);
+  console.log('↓ muxjs.mp4.probe.tracks result');
+  console.table(probeResult);
+  const inspectResult = muxjs.mp4.tools.inspect(bytes);
+  console.log('↓ muxjs.mp4.tools.inspect result');
+  console.log(inspectResult);
 }
 
 async function readVideo(file: File): Promise<ArrayBuffer> {
